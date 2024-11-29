17c9d051f653ba83d31dd1d033a1314a4cfa6a88
Daniel <humbertodanielcaballeros@gmail.com>
2024-11-29 11:38:06 -0500

    obtenerProgramaciones

--- obtenerProgramaciones.js
+++ obtenerProgramaciones.js
@@ -1,21 +1,37 @@
 const AWS = require('aws-sdk');
-const dynamodb = new AWS.DynamoDB.DocumentClient();
-
-const PROGRAMACION_TABLE = process.env.TABLE_NAME_PROGRAMACION;
 
 exports.handler = async () => {
+    console.log('Obteniendo todas las programaciones');
+
     try {
-        const params = { TableName: PROGRAMACION_TABLE };
+        // Validar variables de entorno
+        if (!process.env.TABLE_NAME_PROGRAMACION) {
+            return {
+                statusCode: 500,
+                status: 'Internal Server Error - Variable de entorno TABLE_NAME_PROGRAMACION no configurada',
+            };
+        }
+
+        const tablaProgramacion = process.env.TABLE_NAME_PROGRAMACION;
+
+        // Escanear todas las programaciones en DynamoDB
+        const dynamodb = new AWS.DynamoDB.DocumentClient();
+        const params = { TableName: tablaProgramacion };
+
         const result = await dynamodb.scan(params).promise();
 
+        // Respuesta exitosa
         return {
             statusCode: 200,
-            body: JSON.stringify({ programaciones: result.Items }),
+            message: 'Programaciones obtenidas exitosamente',
+            programaciones: result.Items,
         };
     } catch (error) {
+        console.error(`Error inesperado: ${error.message}`);
         return {
             statusCode: 500,
-            body: JSON.stringify({ message: 'Error al obtener las programaciones' }),
+            status: 'Internal Server Error - Error al obtener las programaciones',
+            error: error.message,
         };
     }
 };
